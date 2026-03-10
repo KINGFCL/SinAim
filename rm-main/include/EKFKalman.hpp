fd8ecf2d579bd4a3488cbd067d0ee86ee73aa3cb
KINGFCL <2867221375@qq.com>
2026-03-10 15:14:03 +0800

    update lingshi

--- rm-main/include/EKFKalman.hpp
+++ rm-main/include/EKFKalman.hpp
@@ -8,8 +8,12 @@
 class EKFKalman
 {
 public:
-//状态向量 State 为 11 维: [xc, yc, zc, vxc, vyc, vzc, theta_0, w, r,l,h]
+
+    /*
+    状态向量 State 为 14 维: [xc, yc, zc, vxc, vyc, vzc, theta_0, w, r,l,h,d_theta_1,d_theta_2,d_theta_3]
+    */
     EKFKalman() = default;
+
     void Init();
 
     // 过程噪声参数
@@ -17,40 +21,57 @@ public:
     const double Var_a = 10000.0, Var_alpha = 0.6;
     const double Var_r = 0.01, Var_l = 0.01, Var_h = 0.01; // 结构参数收敛噪声极小
 
-    // 单装甲板更新
-    Eigen::Matrix<double, 11, 1> operator()(
-        const Eigen::Matrix<double, 11, 1>& State,
+    /*
+        * 单装甲板更新
+        状态向量 State 为 14 维: [xc, yc, zc, vxc, vyc, vzc, theta_0, w, r,l,h,d_theta_1,d_theta_2,d_theta_3]
+        观测向量 View 为 4 维: [x_i, y_i, z_i, yaw_i]
+    */ 
+    Eigen::Matrix<double, 14, 1> operator()(
+        const Eigen::Matrix<double, 14, 1>& State,
         const Eigen::Matrix<double, 4, 1>& View, 
         const cv::Point3d& SCS,
         int armor_id,
         const cv::Quatd& quat,                            
         double dt);
 
-    // 多装甲板序贯更新
-    Eigen::Matrix<double, 11, 1> operator()(
-        const Eigen::Matrix<double, 11, 1>& State,
-        const std::vector<Eigen::Matrix<double, 4, 1>>& Views,
+    /*
+        多装甲板序更新
+        状态向量 State 为 14 维: [xc, yc, zc, vxc, vyc, vzc, theta_0, w, r,l,h,d_theta_1,d_theta_2,d_theta_3]
+        观测向量 View 为 8 维: [x_i, y_i, z_i, yaw_i, x_i+1, y_i+1, z_i+1, yaw_i+1, z_i+1 - z_i , (d_theta_i+1 - d_theta_i)]
+    */ 
+    Eigen::Matrix<double, 14, 1> operator()(
+        const Eigen::Matrix<double, 14, 1>& State,
+        const Eigen::Matrix<double, 10, 1>& Views,
         const cv::Point3d& SCS, 
-        const std::vector<int>& armor_ids,
+        int armor_id,
         const cv::Quatd& quat,                         
         double dt);
         
-    Eigen::Matrix3d getJacobianSphericalToCartesian(const cv::Point3d& SCS);
+    Eigen::Matrix<double, 3, 3> getJacobianSphericalToCartesian(const cv::Point3d& SCS);
 
-    Eigen::Matrix<double, 11, 11> CovState;
+    Eigen::Matrix<double, 4, 14> getStateToViewJacobian(const Eigen::Matrix<double, 14, 1>& X_predict,int armor_id);
+    
+    /*
+    观测向量 View 为 8 维: [x_i, y_i, z_i, yaw_i, x_i+1, y_i+1, z_i+1, yaw_i+1, h , (d_theta_i+1 - d_theta_i)]
+    */
+    Eigen::Matrix<double, 10, 14> getStateToViewsJacobian(const Eigen::Matrix<double, 14, 1>& X_predict,int armor_id);
+
+    Eigen::Matrix<double, 14, 14> CovState;
 
     // 初始化协方差
-    const Eigen::Matrix<double, 11, 11> CovStateInit = (Eigen::Matrix<double, 11, 1>() << 
+    const Eigen::Matrix<double, 14, 14> CovStateInit = (Eigen::Matrix<double, 14, 1>() << 
         100, 100, 100,       // xc, yc, zc 位置方差
         10000, 10000, 10000, // vxc, vyc, vzc 速度方差
         0.01, 36,           // theta, w 角度与角速度方差
-        10, 10, 10           // r, l, h 几何结构初始方差
+        10, 10, 10,           // r, l, h 几何结构初始方差
+        0.0016, 0.0016, 0.0016  //d_theta_1,d_theta_2,d_theta_3
     ).finished().asDiagonal();
 
     // 测量噪声 R
-    Eigen::Matrix<double, 4, 4> CovView = Eigen::Matrix<double, 4, 4>::Zero(); // 4维观测: [x, y, z, yaw] 
+    Eigen::Matrix<double, 4, 4> CovView = Eigen::Matrix<double, 4, 4>::Zero(); // 4维观测: [x, y, z, yaw]
+    Eigen::Matrix<double, 10, 10> CovViews = Eigen::Matrix<double, 10, 10>::Zero(); 
     
-    const Eigen::Matrix<double, 3, 3> CovViewCamera = (Eigen::Matrix<double, 3, 1>() << 
+    Eigen::Matrix<double, 3, 3> CovViewCamera = (Eigen::Matrix<double, 3, 1>() << 
         100, 0.0004, 0.0004  // 相机中球坐标系下的方差，r,theta,phi 的观测噪声
     ).finished().asDiagonal();
 
--- rm-main/src/EKFKalman.cpp
+++ rm-main/src/EKFKalman.cpp
@@ -16,28 +16,12 @@ void EKFKalman::Init()
 // ---------------------------------------------------------
 // 单装甲板更新
 // ---------------------------------------------------------
-Eigen::Matrix<double, 11, 1> EKFKalman::operator()(
-    const Eigen::Matrix<double, 11, 1>& State,
+Eigen::Matrix<double, 14, 1> EKFKalman::operator()(
+    const Eigen::Matrix<double, 14, 1>& State,
     const Eigen::Matrix<double, 4, 1>& View,
     const cv::Point3d& SCS, 
     int armor_id,
     const cv::Quatd& quat, 
-    double dt)
-{
-    std::vector<Eigen::Matrix<double, 4, 1>> Views = {View};
-    std::vector<int> armor_ids = {armor_id};
-    return this->operator()(State, Views, SCS, armor_ids, quat, dt);
-}
-
-// ---------------------------------------------------------
-// 多装甲板序贯更新核心算法
-// ---------------------------------------------------------
-Eigen::Matrix<double, 11, 1> EKFKalman::operator()(
-    const Eigen::Matrix<double, 11, 1>& State,
-    const std::vector<Eigen::Matrix<double, 4, 1>>& Views,
-    const cv::Point3d& SCS,  
-    const std::vector<int>& armor_ids,
-    const cv::Quatd& quat,
     double dt)
 {
     //计算观测噪声矩阵
@@ -54,9 +38,9 @@ Eigen::Matrix<double, 11, 1> EKFKalman::operator()(
     // ==========================================
     // 1. 预测阶段 (Predict) - 纯线性匀速模型
     // ==========================================
-    double vxc = State(3), vyc = State(4), vzc = State(5), w = State(7);
+    const double& vxc = State(3), vyc = State(4), vzc = State(5), w = State(7);
 
-    Eigen::Matrix<double, 11, 1> X_curr = State;
+    Eigen::Matrix<double, 14, 1> X_curr = State;
     X_curr(0) += vxc * dt;
     X_curr(1) += vyc * dt;
     X_curr(2) += vzc * dt;
@@ -64,11 +48,11 @@ Eigen::Matrix<double, 11, 1> EKFKalman::operator()(
     X_curr(6) = std::remainder(X_curr(6), CV_PI * 2.0);
 
     // 状态转移雅可比 F
-    Eigen::Matrix<double, 11, 11> F = Eigen::Matrix<double, 11, 11>::Identity();
+    Eigen::Matrix<double, 14, 14> F = Eigen::Matrix<double, 14, 14>::Identity();
     F(0, 3) = dt; F(1, 4) = dt; F(2, 5) = dt; F(6, 7) = dt;
 
     // 过程噪声 Q
-    Eigen::Matrix<double, 11, 11> Q = Eigen::Matrix<double, 11, 11>::Zero();
+    Eigen::Matrix<double, 14, 14> Q = Eigen::Matrix<double, 14, 14>::Zero();
     auto a = dt * dt * dt * dt * 0.25;  
     auto b = dt * dt * dt * 0.5;       
     auto c = dt * dt;                
@@ -77,112 +61,202 @@ Eigen::Matrix<double, 11, 1> EKFKalman::operator()(
     Q(3,3)=Q(4,4)=Q(5,5) = c * this->Var_a;
     Q(6,6) = a * this->Var_alpha;
     Q(7,7) = c * this->Var_alpha;
-    Q(8,8) = this->Var_r; 
-    Q(9,9) = this->Var_l; 
-    Q(10,10) = this->Var_h; 
+    // Q(8,8) = this->Var_r; 
+    // Q(9,9) = this->Var_l; 
+    // Q(10,10) = this->Var_h; 
 
     Q(0,3) = Q(3,0) = Q(1,4) = Q(4,1) = Q(2,5) = Q(5,2) = b * this->Var_a;     
     Q(6,7) = Q(7,6) = b * this->Var_alpha; 
 
     this->CovState = F * this->CovState * F.transpose() + Q;
 
-    // ==========================================
-    // 预先计算融合权重，并决定序贯更新的优先级
-    // ==========================================
-    std::vector<size_t> update_order(Views.size());
-    std::vector<double> face_projs(Views.size(), 0.1);
+    //获得观测矩阵H
+    Eigen::Matrix<double, 4, 14> H = this->getStateToViewJacobian(X_curr, armor_id);
 
-    for (size_t i = 0; i < Views.size(); ++i) 
-    {
-        update_order[i] = i; // 初始化默认索引
+    //计算卡尔曼增益
+    Eigen::Matrix<double, 14, 4> K = this->CovState * H.transpose() * (H * this->CovState * H.transpose() + this->CovView).inverse();
 
-        Eigen::Matrix<double, 3, 1> P_view{Views[i](0), Views[i](1), 0.0};
-        double distance = P_view.norm();
-        
-        if (distance > 1e-3) {
-            Eigen::Matrix<double, 3, 1> L_view = P_view / distance; 
-            
-            // 切线是 (cos, sin, 0)，逆时针转 90 度的法向量就是 (-sin, cos, 0)
-            Eigen::Matrix<double, 3, 1> N_view(-std::sin(Views[i](3)), std::cos(Views[i](3)), 0.0);
-            // 计算迎角投影，并限制最小值为 0.1 防止除 0
-            face_projs[i] = std::max(std::abs(N_view.dot(L_view)), 0.1); 
-        }
-    }
+    //更新状态
+    int& id = armor_id;
+    const double& pred_xc = X_curr(0);
+    const double& pred_yc = X_curr(1);
+    const double& pred_zc = X_curr(2);
+    const double& pred_yaw = X_curr(6);
+    const double& pred_r = X_curr(8);
+    const double& pred_l = X_curr(9);
+    const double& pred_h = X_curr(10);
 
-    //如果有多块板，优先更新“最正对”的那一块，最多只有 2 块板
-    if (Views.size() == 2) 
-    {
-        if (face_projs[update_order[0]] < face_projs[update_order[1]]) {
-            std::swap(update_order[0], update_order[1]);
-        }
-    }
+    bool is_side = (id == 1 || id == 3);
 
-    // ==========================================
-    // 2. 序贯更新阶段 (Sequential Update)
-    // ==========================================
-    // 按照算好的最优顺序遍历
-    size_t num_views = std::min(Views.size(), (size_t)2);
-    for (size_t order_idx = 0; order_idx < num_views; ++order_idx) 
-    {
-        size_t i = update_order[order_idx]; // 获取实际在 Views 里的索引
-        int id = armor_ids[i];
-        const auto& View = Views[i];
-        double face_proj = face_projs[i]; // 取出预先算好的迎角投影
+    double cur_r = is_side ? (pred_r + pred_l) : pred_r;
+    double cur_z = is_side ? (pred_zc + pred_h) : pred_zc;
 
-        double pred_xc = X_curr(0), pred_yc = X_curr(1), pred_zc = X_curr(2);
-        double pred_yaw = X_curr(6);
-        double pred_r = X_curr(8), pred_l = X_curr(9), pred_h = X_curr(10);
+    double armor_angle = ( (id == 0) ? pred_yaw : pred_yaw + X_curr(10 + id,0) );
+    armor_angle = std::remainder(armor_angle, CV_PI * 2.0);
+    double cos_a = std::cos(armor_angle);
+    double sin_a = std::sin(armor_angle);
 
-        bool is_side = (id == 1 || id == 3);
-        double cur_r = is_side ? (pred_r + pred_l) : pred_r;
-        double cur_z = is_side ? (pred_zc + pred_h) : pred_zc;
+    Eigen::Matrix<double, 4, 1> View_curr;
+    View_curr(0) = pred_xc + cur_r * cos_a;
+    View_curr(1) = pred_yc + cur_r * sin_a;
+    View_curr(2) = cur_z;
+    View_curr(3) = armor_angle;
 
-        double armor_angle = pred_yaw + id * (CV_PI / 2.0);
-        armor_angle = std::remainder(armor_angle, CV_PI * 2.0);
-        double cos_a = std::cos(armor_angle);
-        double sin_a = std::sin(armor_angle);
+    Eigen::Matrix<double, 4, 1> Y = View - View_curr;
+    Y(3) = std::remainder(Y(3), CV_PI * 2.0);
 
-        Eigen::Matrix<double, 4, 1> Z_predict;
-        Z_predict(0) = pred_xc + cur_r * cos_a;
-        Z_predict(1) = pred_yc + cur_r * sin_a;
-        Z_predict(2) = cur_z;
-        Z_predict(3) = armor_angle;
+    Eigen::Matrix<double, 14, 1> X_next = X_curr + K * Y;
 
-        Eigen::Matrix<double, 4, 11> H = Eigen::Matrix<double, 4, 11>::Zero();
-        H(0, 0) = 1.0; H(0, 6) = -cur_r * sin_a; H(0, 8) = cos_a; H(0, 9) = is_side ? cos_a : 0.0; 
-        H(1, 1) = 1.0; H(1, 6) = cur_r * cos_a;  H(1, 8) = sin_a; H(1, 9) = is_side ? sin_a : 0.0; 
-        H(2, 2) = 1.0; H(2, 10) = is_side ? 1.0 : 0.0;  
-        H(3, 6) = 1.0;            
+    X_next(6) = std::remainder(X_next(6), CV_PI * 2.0);
 
-        Eigen::Matrix<double, 4, 1> Y = View - Z_predict;
-        Y(3) = std::remainder(Y(3), CV_PI * 2.0);
-
-        // ==========================================
-        // 自适应观测噪声矩阵 (AEKF)
-        // ==========================================
-        Eigen::Matrix<double, 4, 4> Dynamic_R = this->CovView;
-        double scale = 1.0 / face_proj; // 投影越小，惩罚系数越大
-        
-        Dynamic_R(0, 0) *= scale;             // X: 线性惩罚
-        Dynamic_R(1, 1) *= scale;             // Y: 线性惩罚
-        Dynamic_R(2, 2) *= (scale * scale);   // Z(距离): 大侧偏极度不准，平方惩罚
-        Dynamic_R(3, 3) *= (scale * scale);   // Yaw(角度): 大侧偏极度不准，平方惩罚
-
-        // 计算卡尔曼增益 K (使用动态 R 矩阵)
-        Eigen::Matrix<double, 4, 4> S = H * this->CovState * H.transpose() + Dynamic_R;
-        Eigen::Matrix<double, 11, 4> K = this->CovState * H.transpose() * S.inverse();
-
-        // 更新状态向量
-        X_curr = X_curr + K * Y;
-        X_curr(6) = std::remainder(X_curr(6), CV_PI * 2.0); 
-
-        // 更新协方差矩阵 (使用动态 R 矩阵)
-        Eigen::Matrix<double, 11, 11> I_KH = Eigen::Matrix<double, 11, 11>::Identity() - K * H;
-        this->CovState = I_KH * this->CovState * I_KH.transpose() + K * Dynamic_R * K.transpose();
-    }
-
-    return X_curr;
+    Eigen::Matrix<double, 14, 14> I_KH = Eigen::Matrix<double, 14, 14>::Identity() - K * H;
+    this->CovState = I_KH * this->CovState * I_KH.transpose() + K * this->CovView * K.transpose();
+    return X_next;
 }
+
+
+Eigen::Matrix<double, 14, 1> EKFKalman::operator()(
+    const Eigen::Matrix<double, 14, 1>& State,
+    const Eigen::Matrix<double, 10, 1>& Views,
+    const cv::Point3d& SCS, 
+    int armor_id,
+    const cv::Quatd& quat, 
+    double dt)
+{
+    //计算观测噪声矩阵
+    Eigen::Matrix3d JacobianS2C = this->getJacobianSphericalToCartesian(SCS);
+    Eigen::Matrix3d CovViewCameraCCS = JacobianS2C * this->CovViewCamera * JacobianS2C.transpose(); // 相机坐标系下的观测噪声
+    Eigen::Quaterniond EigenQuat(quat.w, quat.x, quat.y, quat.z);
+    Eigen::Matrix3d R_cam2world = EigenQuat.toRotationMatrix() * this->RCamera2Grip; // 从相机坐标系到世界坐标系的旋转矩阵
+
+    this->CovView.block<3,3>(0,0) = 
+        R_cam2world * CovViewCameraCCS * R_cam2world.transpose(); // 将相机观测噪声转换到世界坐标系
+    
+    this->CovView(3,3) = this->Var_yaw; // yaw 观测噪声
+
+    // ==========================================
+    // 1. 预测阶段 (Predict) - 纯线性匀速模型
+    // ==========================================
+    const double& vxc = State(3), vyc = State(4), vzc = State(5), w = State(7);
+
+    Eigen::Matrix<double, 14, 1> X_curr = State;
+    X_curr(0) += vxc * dt;
+    X_curr(1) += vyc * dt;
+    X_curr(2) += vzc * dt;
+    X_curr(6) += w * dt;
+    X_curr(6) = std::remainder(X_curr(6), CV_PI * 2.0);
+
+    // 状态转移雅可比 F
+    Eigen::Matrix<double, 14, 14> F = Eigen::Matrix<double, 14, 14>::Identity();
+    F(0, 3) = dt; F(1, 4) = dt; F(2, 5) = dt; F(6, 7) = dt;
+
+    // 过程噪声 Q
+    Eigen::Matrix<double, 14, 14> Q = Eigen::Matrix<double, 14, 14>::Zero();
+    auto a = dt * dt * dt * dt * 0.25;  
+    auto b = dt * dt * dt * 0.5;       
+    auto c = dt * dt;                
+    
+    Q(0,0)=Q(1,1)=Q(2,2) = a * this->Var_a;
+    Q(3,3)=Q(4,4)=Q(5,5) = c * this->Var_a;
+    Q(6,6) = a * this->Var_alpha;
+    Q(7,7) = c * this->Var_alpha;
+    // Q(8,8) = this->Var_r; 
+    // Q(9,9) = this->Var_l; 
+    // Q(10,10) = this->Var_h; 
+
+    Q(0,3) = Q(3,0) = Q(1,4) = Q(4,1) = Q(2,5) = Q(5,2) = b * this->Var_a;     
+    Q(6,7) = Q(7,6) = b * this->Var_alpha; 
+
+    this->CovState = F * this->CovState * F.transpose() + Q;
+
+    //获得观测矩阵H
+    Eigen::Matrix<double, 10, 14> H = this->getStateToViewsJacobian(X_curr, armor_id);
+
+    //计算卡尔曼增益
+    Eigen::Matrix<double, 14, 10> K = this->CovState * H.transpose() * (H * this->CovState * H.transpose() + this->CovView).inverse();
+
+    //更新状态
+    int id1 = armor_id;
+    int id2 = (id1 + 1) % 4;
+
+    const double& pred_xc = X_curr(0);
+    const double& pred_yc = X_curr(1);
+    const double& pred_zc = X_curr(2);
+    const double& pred_yaw = X_curr(6);
+    const double& pred_r = X_curr(8);
+    const double& pred_l = X_curr(9);
+    const double& pred_h = X_curr(10);
+
+    bool is_side1 = (id1 == 1 || id1 == 3);
+    bool is_side2 = (id2 == 1 || id2 == 3);
+
+    double cur_r1 = is_side1 ? (pred_r + pred_l) : pred_r;
+    double cur_z1 = is_side1 ? (pred_zc + pred_h) : pred_zc;
+
+    double cur_r2 = is_side2 ? (pred_r + pred_l) : pred_r;
+    double cur_z2 = is_side2 ? (pred_zc + pred_h) : pred_zc;
+
+    double armor_angle1 = ( (id1 == 0) ? pred_yaw : pred_yaw + X_curr(10 + id1,0) );
+    armor_angle1 = std::remainder(armor_angle1, CV_PI * 2.0);
+    double cos_a1 = std::cos(armor_angle1);
+    double sin_a1 = std::sin(armor_angle1);
+
+    double armor_angle2 = ( (id2 == 0) ? pred_yaw : pred_yaw + X_curr(10 + id2,0) );
+    armor_angle2 = std::remainder(armor_angle2, CV_PI * 2.0);
+    double cos_a2 = std::cos(armor_angle2);
+    double sin_a2 = std::sin(armor_angle2);
+
+    Eigen::Matrix<double, 4, 1> View_curr1;
+    View_curr1(0) = pred_xc + cur_r1 * cos_a1;
+    View_curr1(1) = pred_yc + cur_r1 * sin_a1;
+    View_curr1(2) = cur_z1;
+    View_curr1(3) = armor_angle1;
+
+    Eigen::Matrix<double, 4, 1> View_curr2;
+    View_curr2(0) = pred_xc + cur_r2 * cos_a2;
+    View_curr2(1) = pred_yc + cur_r2 * sin_a2;
+    View_curr2(2) = cur_z2;
+    View_curr2(3) = armor_angle2;
+
+    Eigen::Matrix<double, 10, 1> Views_curr;
+    Views_curr.block<4,1>(0,0) = View_curr1;
+    Views_curr.block<4,1>(4,0) = View_curr2;
+    if(id1 == 0)
+    {
+        Views_curr(8,0) = pred_h;
+    }
+    
+
+    if(id1 == 0)
+    {
+        Views_curr(9,0) = X_curr(11,0);
+    }
+    else if(id1 == 3)
+    {
+        Views_curr(9,0) = -X_curr(13,0);
+    }
+    else {
+        Views_curr(9,0) = X_curr(10+id2,0) - X_curr(10 + id1,0);
+    }
+    
+
+    Eigen::Matrix<double, 10, 1> Y = Views -Views_curr;
+    Y(3,0) = std::remainder(Y(3), CV_PI * 2.0);
+    Y(7,0) = std::remainder(Y(7), CV_PI * 2.0);
+    Y(9,0) = std::remainder(Y(9), CV_PI * 2.0);
+
+    Eigen::Matrix<double, 14, 1> X_next = X_curr + K * Y;
+
+    X_next(6) = std::remainder(X_next(6), CV_PI * 2.0);
+    X_next(11) = std::remainder(X_next(11), CV_PI * 2.0);
+    X_next(12) = std::remainder(X_next(12), CV_PI * 2.0);
+    X_next(13) = std::remainder(X_next(13), CV_PI * 2.0);
+
+    Eigen::Matrix<double, 14, 14> I_KH = Eigen::Matrix<double, 14, 14>::Identity() - K * H;
+    this->CovState = I_KH * this->CovState * I_KH.transpose() + K * this->CovView * K.transpose();
+    return X_next;
+}
+
 
 
 /**
@@ -191,7 +265,7 @@ Eigen::Matrix<double, 11, 1> EKFKalman::operator()(
  * 基于 OpenCV 相机系约定：Z向前，X向右，Y向下
  * @return Eigen::Matrix3d 返回 3x3 的雅可比矩阵 J
  */
-Eigen::Matrix3d EKFKalman::getJacobianSphericalToCartesian(const cv::Point3d& SCS) {
+Eigen::Matrix<double, 3, 3> EKFKalman::getJacobianSphericalToCartesian(const cv::Point3d& SCS) {
     // SCS.x = r (距离)
     // SCS.y = theta (极角，与Z轴夹角)
     // SCS.z = phi (方位角，XY平面夹角)
@@ -204,7 +278,7 @@ Eigen::Matrix3d EKFKalman::getJacobianSphericalToCartesian(const cv::Point3d& SC
     double sp = std::sin(phi);
     double cp = std::cos(phi);
 
-    Eigen::Matrix3d J;
+    Eigen::Matrix<double, 3, 3> J;
 
     // 第一行：∂x/∂r, ∂x/∂theta, ∂x/∂phi
     J(0, 0) = st * cp;               // sin(theta) * cos(phi)
@@ -222,4 +296,74 @@ Eigen::Matrix3d EKFKalman::getJacobianSphericalToCartesian(const cv::Point3d& SC
     J(2, 2) = 0.0;                   // z轴坐标与phi无关
 
     return J;
+}
+
+Eigen::Matrix<double, 4, 14> EKFKalman::getStateToViewJacobian(const Eigen::Matrix<double, 14, 1>& X_predict, int armor_id)
+{
+    Eigen::Matrix<double, 4, 14> H = Eigen::Matrix<double, 4, 14>::Zero();
+
+    const double& yaw_0 = X_predict(6);
+    const double& r = X_predict(8);
+    const double& l = X_predict(9);
+
+    bool is_side = (armor_id == 1 || armor_id == 3);
+    double r_i = is_side ? (r + l) : r;
+
+    // 获取当前板的物理角度 (虽然这里用到了 delta_theta，但不影响我们切断它的求导链条！)
+    double theta_i = (armor_id == 0) ? yaw_0 : yaw_0 + X_predict(10 + armor_id);
+    
+    double cos_theta = std::cos(theta_i);
+    double sin_theta = std::sin(theta_i);
+
+    // 1. 位置对中心坐标求导
+    H(0, 0) = 1.0;
+    H(1, 1) = 1.0;
+    H(2, 2) = 1.0;
+
+    // 2. 位置对结构参数求导
+    H(0, 8) = cos_theta;                  // 对 r
+    H(1, 8) = sin_theta;                  // 对 r
+    H(0, 9) = is_side ? cos_theta : 0.0;  // 对 l
+    H(1, 9) = is_side ? sin_theta : 0.0;  // 对 l
+    H(2, 10) = is_side ? 1.0 : 0.0;       // 对 h
+
+    // 3. 对全局 yaw_0 求导
+    H(0, 6) = -r_i * sin_theta;
+    H(1, 6) =  r_i * cos_theta;
+    H(3, 6) = 1.0;
+
+    return H;
+}
+
+Eigen::Matrix<double, 10, 14> EKFKalman::getStateToViewsJacobian(const Eigen::Matrix<double, 14, 1>& X_predict, int armor_id)
+{
+    Eigen::Matrix<double, 10, 14> H2 = Eigen::Matrix<double, 10, 14>::Zero();
+
+    int id1 = armor_id;
+    int id2 = (armor_id + 1)%4;
+    H2.block<4, 14>(0, 0) = this->getStateToViewJacobian(X_predict, id1);
+    H2.block<4, 14>(4, 0) = this->getStateToViewJacobian(X_predict, id2);
+    
+    // 第 9 维：高度差约束 h 
+    // (预测值就是状态量 h, 索引为 10，对其导数为 1)
+    H2(8, 10) = 1.0;
+
+    // 第 10 维：相对偏角差约束 (DeltaTheta_id2 - DeltaTheta_id1)
+    // (只对这俩特定的装配偏角有非 0 导数，彻底和 yaw_0 解耦！)
+    if(id1 == 0)
+    {
+        H2(9, 10 + id2) = 1.0;
+    }
+    else if(id1 == 3)
+    {
+        H2(9, 13) = -1.0;
+    }
+    else 
+    {
+        H2(9, 10 + id1) = -1.0;
+        H2(9, 10 + id2) = 1.0;
+    }
+
+
+    return H2;
 }
