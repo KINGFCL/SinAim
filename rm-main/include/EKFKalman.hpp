1892f35dc99f67431871c48a71ba1984110f4295
KING <2867221375@qq.com>
2026-04-24 14:14:07 +0800

    这一版是完全可以用的

--- rm-main/include/EKFKalman.hpp
+++ rm-main/include/EKFKalman.hpp
@@ -25,7 +25,7 @@ public:
     const double Var_r = 100.0, Var_yaw = 0.01, Var_dtheta = 0.0001; // 观测yaw值方差
     
     // 过程噪声参数
-    const double Var_a_xy = 10000.0,  Var_a_z = 10.0, Var_alpha = 1;
+    const double Var_a_xy = 10000.0,  Var_a_z = 10.0, Var_alpha = 10;
     
     // 从相机坐标系到手坐标系的旋转矩阵
     const Eigen::Matrix<double, 3, 3> RCamera2Grip
