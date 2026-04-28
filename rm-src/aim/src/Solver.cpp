43302dbb28c7428840a5c1ab7b07e06568954f86
KING <2867221375@qq.com>
2026-04-28 22:23:49 +0800

    准备跑路了

--- rm-src/aim/src/Solver.cpp
+++ rm-src/aim/src/Solver.cpp
@@ -1,7 +1,8 @@
 #include "Solver.hpp"
 #include "Armor.hpp"
 #include "eigen3/Eigen/Dense"
-#include <Eigen/src/Core/Matrix.h>
+#include <iostream>
+
 #include <array>
 #include <cmath>
 #include <opencv2/core.hpp>
@@ -24,11 +25,12 @@ Solver::Solver( const SolverConfig& config ):
     T_Cam_to_gripper(Eigen::Map<const Eigen::Matrix<double, 3, 1> >(config.T_Cam_to_gripper.data()) ),
     reproj_threshold(config.reproj_threshold)
 {
-    float hs = static_cast<float>(h) / 2.0f;
-    float ws = static_cast<float>(w_small) / 2.0f;
-    float wb = static_cast<float>(w_big)   / 2.0f;
-    objectSmallArmorP = { {-ws, -hs, 0}, {ws, -hs, 0}, {ws, hs, 0}, {-ws, hs, 0} };
-    objectBigArmorP   = { {-wb, -hs, 0}, {wb, -hs, 0}, {wb, hs, 0}, {-wb, hs, 0} };
+    double hs = this->h * 0.5;
+    double ws = w_small * 0.5;
+    double wb = w_big * 0.5;
+
+    objectSmallArmorP = { {ws, hs, 0.0}, {-ws,hs, 0.0}, {-ws,-hs, 0.0}, {ws, -hs, 0.0} };
+    objectBigArmorP   = { {0, wb, hs}, {0.0,-wb,hs}, {0, -wb, -hs}, {0, wb, -hs} };
 }
 
 std::vector< std::array<ArmorPosi,2> > Solver::operator () (const std::vector<CVArmor>& armors, const Eigen::Quaterniond& gripper_to_world)
@@ -51,10 +53,11 @@ std::vector< std::array<ArmorPosi,2> > Solver::operator () (const std::vector<CV
         {
             std::vector<cv::Mat> rvecs, tvecs;
             std::vector<double> reprojErr;
-            cv::solvePnPGeneric(objectSmallArmorP, armor.Lightcorners, cameraMatrix, distCoeffs,
+            cv::solvePnPGeneric(objectSmallArmorP, armor.Lightcorners, this->cameraMatrix, this->distCoeffs,
                                 rvecs, tvecs, false, cv::SOLVEPNP_IPPE,
                                 cv::noArray(), cv::noArray(), reprojErr);
             for(int i = 0; i < 2; i++) {
+
                 cv::Mat R_cv;
                 cv::Rodrigues(rvecs[i], R_cv);
                 Eigen::Matrix3d R_armor;
@@ -63,7 +66,7 @@ std::vector< std::array<ArmorPosi,2> > Solver::operator () (const std::vector<CV
                         R_armor(r,c) = R_cv.at<double>(r,c);
                 Eigen::Vector3d T_cam(tvecs[i].at<double>(0), tvecs[i].at<double>(1), tvecs[i].at<double>(2));
                 center_small.col(i) = R_cam2world * T_cam + photocenter_world;
-                Eigen::Vector3d toward_world = R_cam2world * (R_armor * Eigen::Vector3d(1,0,0));
+                Eigen::Vector3d toward_world = R_cam2world * (R_armor * Eigen::Vector3d(0,0,1));
                 yaw_small[i] = std::atan2(toward_world.y(), toward_world.x());
                 reproj_small[i] = reprojErr[i];
             }
@@ -89,7 +92,7 @@ std::vector< std::array<ArmorPosi,2> > Solver::operator () (const std::vector<CV
                         R_armor(r,c) = R_cv.at<double>(r,c);
                 Eigen::Vector3d T_cam(tvecs[i].at<double>(0), tvecs[i].at<double>(1), tvecs[i].at<double>(2));
                 center_big.col(i) = R_cam2world * T_cam + photocenter_world;
-                Eigen::Vector3d toward_world = R_cam2world * (R_armor * Eigen::Vector3d(1,0,0));
+                Eigen::Vector3d toward_world = R_cam2world * (R_armor * Eigen::Vector3d(0,0,1));
                 yaw_big[i] = std::atan2(toward_world.y(), toward_world.x());
                 reproj_big[i] = reprojErr[i];
             }
