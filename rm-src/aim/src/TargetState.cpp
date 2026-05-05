c74b8cc93fc213a7d7b347992059729ff6169e5f
KINGFCL <2867221375@qq.com>
2026-05-05 15:21:50 +0800

    修改了TargetState的返回数据的单位

--- rm-src/aim/src/TargetState.cpp
+++ rm-src/aim/src/TargetState.cpp
@@ -44,7 +44,7 @@ Eigen::Matrix<double, 4, 4> RobotState::Predict(double dt)const
     ans.block<3,1>(0,2) += move;
     ans.block<3,1>(0,3) += move;
 
-    return ans;
+    return ans*0.01;// 转换为米单位
 }
 
 // 补全缺失的根据时间点推演方法
